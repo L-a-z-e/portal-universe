1e7e612f5bccb189b097e72c6dd606a678af7507
L-a-z-e <yysi8771@gmail.com>
2025-09-09 00:43:23 +0900

    portal-universe first commit

---
+++ services/blog-service/src/test/java/com/portal/universe/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.universe.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/discovery-service/src/test/java/com/portal/universe/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.universe.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
