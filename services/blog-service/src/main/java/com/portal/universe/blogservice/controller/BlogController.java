335e93b04d16beb2bd198c15cecb7bbbca564faf
L-a-z-e <yysi8771@gmail.com>
2025-09-20 21:28:08 +0900

    blog-service naming rule refactoring

--- services/blog-service/src/main/java/com/portal/universe/blogservice/controller/BlogController.java
+++ services/blog-service/src/main/java/com/portal/universe/blogservice/controller/BlogController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/blogs")
+@RequestMapping("/api/blog")
 @RequiredArgsConstructor
 public class BlogController {
 
