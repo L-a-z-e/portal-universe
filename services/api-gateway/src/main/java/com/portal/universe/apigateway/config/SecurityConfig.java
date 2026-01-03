29e454bf9d45ea4f44c75bf9a84b7782d2455ee9
L-a-z-e <yysi8771@gmail.com>
2026-01-03 23:24:16 +0900

    feature: SecurityConfig test profile 분리

--- services/api-gateway/src/main/java/com/portal/universe/apigateway/config/SecurityConfig.java
+++ services/api-gateway/src/main/java/com/portal/universe/apigateway/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
@@ -31,6 +32,7 @@ import java.util.List;
  */
 @Configuration
 @EnableWebFluxSecurity
+@Profile("!test")
 @Slf4j
 public class SecurityConfig {
 
