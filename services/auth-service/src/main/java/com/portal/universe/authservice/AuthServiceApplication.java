5dfaa9f2ba7f66dd0a4ce84c5dc37b4293b61ac9
L-a-z-e <yysi8771@gmail.com>
2026-01-16 16:44:52 +0900

    feature: auth-architecture-redesign (Separation of Concerns)

--- services/auth-service/src/main/java/com/portal/universe/authservice/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/portal/universe/authservice/AuthServiceApplication.java
@@ -3,11 +3,13 @@ package com.portal.universe.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * 인증(Auth) 서비스의 메인 애플리케이션 클래스입니다.
  * Spring Boot 애플리케이션의 진입점(Entry Point) 역할을 합니다.
  */
+@EnableJpaAuditing
 @SpringBootApplication
 // 공통 라이브러리(common-library)에 정의된 Bean들을 스캔하기 위해 패키지 범위를 명시적으로 지정합니다.
 @ComponentScan(basePackages = { "com.portal.universe.authservice", "com.portal.universe.commonlibrary" })
