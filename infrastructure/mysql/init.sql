003eb6e9ef42cbb6ea1e01d0a55ba396c1dcf63f
L-a-z-e <yysi8771@gmail.com>
2026-02-01 16:43:22 +0900

    refactor(api-gateway): notification_db database creation and grant permissions

--- infrastructure/mysql/init.sql
+++ infrastructure/mysql/init.sql
@@ -6,8 +6,10 @@
 -- 데이터베이스 생성
 CREATE DATABASE IF NOT EXISTS auth_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS shopping_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS notification_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- 사용자 권한 부여
 GRANT ALL PRIVILEGES ON auth_db.* TO 'laze'@'%';
 GRANT ALL PRIVILEGES ON shopping_db.* TO 'laze'@'%';
+GRANT ALL PRIVILEGES ON notification_db.* TO 'laze'@'%';
 FLUSH PRIVILEGES;
